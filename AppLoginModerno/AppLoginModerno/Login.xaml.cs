7295729ee4e8062b522ec75fd898567413b0c9dd
TIAGO SILVA <frost.tiago@gmail.com>
2021-05-05 11:38:31 -0300

    Adicionar arquivos de projeto.

---
+++ AppLoginModerno/AppLoginModerno/Login.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppLoginModerno
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new Protegido();
+        }
+    }
+}
---
+++ AppLoginModerno/AppLoginModerno/Protegido.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppLoginModerno
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Protegido : ContentPage
+    {
+        public Protegido()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            bool confirmacao = await DisplayAlert("Tem certeza?", "Sair do App?", "Sim", "Não");
+
+            if(confirmacao)
+                App.Current.MainPage = new Login();
+        }
+    }
+}
